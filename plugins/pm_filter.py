39da191f139dc7027013ce0866e8fde55338b75d
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-02-24 20:21:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -98,7 +98,7 @@ async def pm_text(bot, message):
                 if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
                 if PM_SEARCH == True:
                     ai_search = True
-                    reply_msg = await bot.send_message(message.from_user.id, f"<b><i>Searching For {content} 🔍</i></b>", reply_to_message_id=message.id)
+                    reply_msg = await bot.send_message(message.from_user.id, f"<b><i>Searching For your requested movie/series🔍</i></b>", reply_to_message_id=message.id)
                     await auto_filter(bot, content, message, reply_msg, ai_search)
                 else:
                     await message.reply_text(
