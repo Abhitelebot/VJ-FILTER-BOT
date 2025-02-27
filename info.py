3ea67a8efabc0b0e36185624ef380cda1de0c3c1
Abhitelebot <161284617+Abhitelebot@users.noreply.github.com>
2025-02-27 20:20:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -94,7 +94,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'publicearn.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'ec9f84376253600bbe85cb8664c9bd9a1ba846c7)
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'ec9f84376253600bbe85cb8664c9bd9a1ba846c7')
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
@@ -103,7 +103,7 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/A2Z_how_to_verify
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE',True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'ec9f84376253600bbe85cb8664c9bd9a1ba846c7)
+SHORTLINK_API = environ.get('SHORTLINK_API', 'ec9f84376253600bbe85cb8664c9bd9a1ba846c7')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
